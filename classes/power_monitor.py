4ec177837dbfa4b811c66aca71625ffb587b9684
Karzan <karzan.maher@gmail.com>
2025-05-12 00:25:16 +0200

    update calculations

--- classes/power_monitor.py
+++ classes/power_monitor.py
@@ -49,33 +49,38 @@ class PowerMonitor:
         if len(parts) != 2:
             return None, np.array([])
         try:
-            data = np.array([ (3.3*float(x)/4095) for x in parts[1].split(',') if x.strip() != ''])
+            if parts[0] != 'ch1':
+                data = np.array([ (3.3*float(x)/4095) for x in parts[1].split(',') if x.strip() != ''])
+                # convert voltage reading from current sensor: 100mv = 1A
+                data = np.divide(data, 0.1)
+            else:
+                data = np.array([[(float(x)) for x in parts[1].split(',') if x.strip() != '']])
+                tmp = np.mean(data)
+                data = [1625*(3.3*(x-tmp)/4096) for x in data]
         except ValueError:
             return None, np.array([])
         
-        if parts[0] != 'ch1':
-            # convert voltage reading from current sensor: 100mv = 1A
-            data = np.divide(data, 0.1)
         return parts[0], data
 
     def calc_power(self, ch):
         if ch == "ch1":
-            self.channel_data_result[ch]["Urms"] = round(np.sum(self.channel_data_raw[ch]["Urms"]), self.ROUND_NUM)
+            self.channel_data_result[ch]["Urms"] = np.sum(self.channel_data_raw[ch]["Urms"]) / len(self.channel_data_raw[ch]["Urms"])
+            self.channel_data_result[ch]["Urms"] = round(self.channel_data_result, self.ROUND_NUM)
             return
 
         Irms_array = np.asarray(self.channel_data_raw[ch]["Irms"])
         P_array = np.asarray(self.channel_data_raw[ch]["P"])
         Urms_val = self.channel_data_result["ch1"]["Urms"]
 
-        Irms_sum = np.sum(Irms_array)
-        P_sum = np.sum(P_array)
-        S = Urms_val * Irms_sum
-        Q_mag = np.sqrt(abs(S**2 - P_sum**2))
-        PF = P_sum / S if S != 0 else None
+        Irms_m = np.sum(Irms_array) / len(Irms_array)
+        P_m = np.sum(P_array) / len(P_array)
+        S = Urms_val * Irms_m
+        Q_mag = np.sqrt(abs(S**2 - P_m**2))
+        PF = P_m / S if S != 0 else None
 
         self.channel_data_result[ch].update({
-            "Irms": round(Irms_sum, self.ROUND_NUM),
-            "P": round(P_sum, self.ROUND_NUM),
+            "Irms": round(Irms_m, self.ROUND_NUM),
+            "P": round(P_m, self.ROUND_NUM),
             "S": round(S, self.ROUND_NUM),
             "Q": round(Q_mag, self.ROUND_NUM),
             "PF": round(PF, self.ROUND_NUM)
@@ -99,6 +104,9 @@ class PowerMonitor:
         for ch in self.channel_data_raw:
             for key in self.channel_data_raw[ch]:
                 self.channel_data_raw[ch][key].clear()
+        for ch in self.channel_data_result:
+            for key in self.channel_data_result[ch]:
+                self.channel_data_result[ch][key] = 0
 
     def display_monitor(self):
         for ch, vals in self.channel_data_result.items():
